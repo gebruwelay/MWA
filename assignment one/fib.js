bf6223516f5ad18965f4a1a213951257bfe5a451
Berhie <berhie@Zeab.local>
2021-09-27 19:10:33 -0500

    fibonanchi

---
+++ assignment one/fib.js
@@ -0,0 +1,13 @@
+const fib= (n) =>{
+    if(n==1)
+    {
+        return 1;
+    }
+    if (n==0)
+     return 0;
+    return fib(n-2)+fib(n-1);
+}
+
+module.exports = {
+    fibonanchi : fib
+}
---
+++ assignment one/index.js
@@ -0,0 +1,2 @@
+const fib = require("./fib");
+console.log (fib.fibonanchi(Math.abs(-30)));
